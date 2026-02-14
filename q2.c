ca670852968dec49ff4b9861b669de7c3be7383e
Rishi2914-cloud <rishibohra2914@gmail.com>
2026-02-14 15:15:20 +0530

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
+{
+    
+    *returnSize = 2;
+
+    
+    int* answer = (int*)malloc(2 * sizeof(int));
+
+    
+    for(int i = 0; i < numsSize; i++)
+    {
+        for(int j = i + 1; j < numsSize; j++)
+        {
+            if(nums[i] + nums[j] == target)
+            {
+                answer[0] = i;
+                answer[1] = j;
+                return answer;   
+            }
+        }
+    }
+
+    return NULL;  
+}
+
+
