11f4669c697db1bfb10101c088176fd1a35a0d5b
Rishi2914-cloud <rishibohra2914@gmail.com>
2026-02-15 19:26:15 +0530

    Add files via upload

---
+++ q17.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char str[], int index)
+{
+    if (str[index] == '\0')
+        return;
+
+    reverse(str, index + 1);
+
+    printf("%c", str[index]);
+}
+
+int main()
+{
+    char str[100];
+
+    printf("Enter code name: ");
+    scanf("%s", str);
+
+    printf("Mirror format: ");
+    reverse(str, 0);
+
+    return 0;
+}
