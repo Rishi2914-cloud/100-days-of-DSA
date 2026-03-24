4719a23521eac3e9e90c8c7a5e1896925e53360e
Rishi2914-cloud <rishibohra2914@gmail.com>
2026-03-24 19:38:26 +0530

    Add files via upload

---
+++ q62.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+
+bool isValid(char* s) {
+    char stack[10000];
+    int top = -1;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        char ch = s[i];
+
+        if (ch == '(' || ch == '{' || ch == '[') {
+            stack[++top] = ch;
+        } else {
+            if (top == -1) return false;
+
+            char t = stack[top--];
+
+            if ((ch == ')' && t != '(') ||
+                (ch == '}' && t != '{') ||
+                (ch == ']' && t != '[')) {
+                return false;
+            }
+        }
+    }
+
+    return top == -1;
+}
